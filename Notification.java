799cefcbe87b202893c67d9ed6b1c10b708adf7b
RahmaYasser <49435053+RahmaYasser@users.noreply.github.com>
2020-12-26 18:03:02 +0200

    Delete Notification.java

--- Notification.java
+++
@@ -1,9 +0,0 @@
-public abstract class Notification {
-    String Content = null;
-
-    public void prepareContent(String tmplateMessage, String[] Placeholders) {
-
-    }
-    public abstract void sendNotification();
-
-}
