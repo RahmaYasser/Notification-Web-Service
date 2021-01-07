9f717fe888e33a84c764243b7071d4f5862c2bbc
HagarAbdelzaher <50349835+HagarAbdelzaher@users.noreply.github.com>
2021-01-07 20:32:21 +0200

    Update EmailNotification.java

--- demo/src/main/java/com/software/demo/EmailNotification.java
+++ demo/src/main/java/com/software/demo/EmailNotification.java
@@ -1,13 +1,11 @@
 package com.software.demo;
-
-
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class EmailNotification extends Notification {
     
     public EmailNotification() {}
-    
+  
     public boolean validateContact() {
     	String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
