0c66e0d9fed9a4fdefeeae8f61d9968424147237
RahmaYasser <49435053+RahmaYasser@users.noreply.github.com>
2021-01-07 21:08:28 +0200

    Update EmailNotification.java

--- demo/src/main/java/com/software/demo/EmailNotification.java
+++ demo/src/main/java/com/software/demo/EmailNotification.java
@@ -6,6 +6,7 @@ public class EmailNotification extends Notification {
     
     public EmailNotification() {}
   
+    // this function to check if the email exists
     public boolean validateContact() {
     	String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
