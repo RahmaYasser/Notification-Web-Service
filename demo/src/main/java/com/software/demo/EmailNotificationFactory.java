861d22ae1d0c570b89f0c5902a9b511e40f9f57f
HagarAbdelzaher <50349835+HagarAbdelzaher@users.noreply.github.com>
2021-01-07 20:33:09 +0200

    Update EmailNotificationFactory.java

--- demo/src/main/java/com/software/demo/EmailNotificationFactory.java
+++ demo/src/main/java/com/software/demo/EmailNotificationFactory.java
@@ -3,20 +3,8 @@ package com.software.demo;
 import java.util.ArrayList;
 
 public class EmailNotificationFactory implements INotificationFactory{
-/*	
-	public boolean checkEmail(String email ) {
-		
-		  String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-          java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-          java.util.regex.Matcher m = p.matcher(email);
-          return m.matches();
-
-
-	} */
-
 @Override
 public Notification CreateNotification(String[] placeholders ,String message,String Contact) {
-		
 	Notification notification = new EmailNotification();
 	if(notification.placeholdersSizeValidation(placeholders.length, message))
 	{	
@@ -28,7 +16,6 @@ public Notification CreateNotification(String[] placeholders ,String message,Str
 		System.out.println("Invalid placeholders");
 		return null;
 	}
-	
 	return notification;
 	}
 	
