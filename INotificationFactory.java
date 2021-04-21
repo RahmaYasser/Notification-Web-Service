350926255cf0b16e6028c77c5d9829d273af16e9
RahmaYasser <49435053+RahmaYasser@users.noreply.github.com>
2021-04-21 22:57:38 +0200

    Delete INotificationFactory.java

--- INotificationFactory.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public interface INotificationFactory {
-    public Notification CreateNotification(ArrayList<String> placeholders,String Language,String templateType,String Contact);
-}
